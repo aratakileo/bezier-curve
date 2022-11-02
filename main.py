c23667221cbe340b13ead1c73ece0f942f91c13a
TeaCondemns <tea.condemns@gmail.com>
2022-11-02 18:19:46 +0300

    Recovery

--- main.py
+++ main.py
@@ -1,44 +1,70 @@
-from arcade import gui
-import arcade as arc
+from pygame import display
+from math import sqrt
+import pygame
 
 
-class Window(arc.Window):
-    def __init__(self):
-        super().__init__(800, 800, 'Graphic value', resizable=True)
-        self.background_color = arc.color.WHITE
-        self.button = gui.UIFlatButton(text="Test", width=200)
+def draw_parabola(_surface, _x, _y, _width, _height):
+    points = []
 
-        self.ui_manager = gui.UIManager()
-        self.ui_manager.enable()
-        self.ui_manager.add(
-            gui.UIAnchorWidget(
-                anchor_x='center_x',
-                anchor_y='center_y',
-                child=self.button
-            )
-        )
+    print(_width, _height)
+    k = -1 if _height < 0 else 1
+    # _width = abs(_width)
+    # _height = abs(_height)
 
-    def on_draw(self):
-        arc.start_render()
-        arc.draw_circle_outline(*self.get_center(), 400, arc.color.RED, num_segments=1000)
-        self.ui_manager.draw()
+    for ix in range(0, _width * 2):
+        iy = k * ((ix - _width / 2) / 15) ** 2
 
-    def on_mouse_drag(self, x: int, y: int, dx: int, dy: int, buttons: int, modifiers: int):
-        def on_event(event):
-            print(event)
+        if iy > _height:
+            continue
 
-        self.button.on_event = on_event
+        points += [(_x + ix, _y + iy)]
 
-    def get_center_x(self):
-        return self._width / 2
+    print(points)
+    if len(points) > 1:
+        pygame.draw.lines(_surface, 0xf44336, False, points)
 
-    def get_center_y(self):
-        return self._height / 2
-
-    def get_center(self):
-        return self.get_center_x(), self.get_center_y()
+    pygame.draw.line(_surface, 0x4caf50, (_x, _y), (_x + _width, _y + _height))
+    pygame.draw.line(_surface, 0x4caf50, (_x + _width / 2, _y), (_x + _width / 2, _y + _height))
 
 
-app = Window()
+pygame.init()
+display.set_caption("Graphic value")
+display.set_mode((800, 800), pygame.RESIZABLE)
 
-arc.run()
+surface = display.get_surface()
+clock = pygame.time.Clock()
+
+pos = [0, 200]
+point = [400, 400]
+point_r = 12
+point_click = [False, False]
+
+while True:
+    mouse_point = pygame.mouse.get_pos()
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            exit()
+
+        if e.type == pygame.MOUSEBUTTONDOWN and e.button == pygame.BUTTON_LEFT:
+            distance = sqrt((pos[0] + point[0] - mouse_point[0]) ** 2 + (pos[1] + point[1] - mouse_point[1]) ** 2)
+
+            if distance > point_r and not point_click[1]:
+                point_click[0] = True
+
+            if distance <= point_r and not point_click[0]:
+                point_click[1] = True
+        elif e.type == pygame.MOUSEBUTTONUP and e.button == pygame.BUTTON_LEFT:
+            point_click = [False, False]
+
+    if point_click[1]:
+        point = [mouse_point[0] - pos[0], mouse_point[1] - pos[1]]
+
+    surface.fill(0xffffff)
+
+    draw_parabola(surface, *pos, *point)
+
+    pygame.draw.circle(surface, 0x4caf50, (pos[0] + point[0], pos[1] + point[1]), point_r)
+
+    display.flip()
+    clock.tick(60)
