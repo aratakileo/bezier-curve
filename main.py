a4d78220756cf5359d1836fa5da11987d97855d7
TeaCondemns <tea.condemns@gmail.com>
2022-11-04 22:38:02 +0300

    Update main.py
    Getting closer to expecting result of final functionality

--- main.py
+++ main.py
@@ -1,70 +1,102 @@
-from pygame import display
+from pygame import display, font, mouse
 from math import sqrt
 import pygame
 
 
-def draw_parabola(_surface, _x, _y, _width, _height):
-    points = []
-
-    print(_width, _height)
-    k = -1 if _height < 0 else 1
-    # _width = abs(_width)
-    # _height = abs(_height)
-
-    for ix in range(0, _width * 2):
-        iy = k * ((ix - _width / 2) / 15) ** 2
-
-        if iy > _height:
-            continue
-
-        points += [(_x + ix, _y + iy)]
-
-    print(points)
-    if len(points) > 1:
-        pygame.draw.lines(_surface, 0xf44336, False, points)
-
-    pygame.draw.line(_surface, 0x4caf50, (_x, _y), (_x + _width, _y + _height))
-    pygame.draw.line(_surface, 0x4caf50, (_x + _width / 2, _y), (_x + _width / 2, _y + _height))
-
-
 pygame.init()
+font.init()
+
 display.set_caption("Graphic value")
 display.set_mode((800, 800), pygame.RESIZABLE)
 
 surface = display.get_surface()
 clock = pygame.time.Clock()
+basefont = font.Font(None, 20)
 
-pos = [0, 200]
-point = [400, 400]
-point_r = 12
-point_click = [False, False]
+is_pressed = False
+start, stop = 0, 113
+
+colors = (0xf44336, 0x3f51b5, 0xffc107)
+is_num_pressed = [False, False, False]
+coefficients = [(2.1, 0.15), (None, None), (2.1, 0.15)]
 
 while True:
-    mouse_point = pygame.mouse.get_pos()
+    mouse_pos = mouse.get_pos()
 
     for e in pygame.event.get():
         if e.type == pygame.QUIT:
             exit()
 
-        if e.type == pygame.MOUSEBUTTONDOWN and e.button == pygame.BUTTON_LEFT:
-            distance = sqrt((pos[0] + point[0] - mouse_point[0]) ** 2 + (pos[1] + point[1] - mouse_point[1]) ** 2)
+        if e.type == pygame.MOUSEBUTTONDOWN and e.button == pygame.BUTTON_MIDDLE:
+            is_pressed = True
+        elif e.type == pygame.MOUSEBUTTONUP and e.button == pygame.BUTTON_MIDDLE:
+            is_pressed = False
 
-            if distance > point_r and not point_click[1]:
-                point_click[0] = True
+            if True in is_num_pressed:
+                print(mouse_pos)
+        elif e.type == pygame.KEYDOWN:
+            is_num_pressed = [e.key == key for key in (pygame.K_1, pygame.K_2, pygame.K_3)]
+        elif e.type == pygame.KEYUP:
+            # if e.key == pygame.K_F1:
+            #     kx, ky = float(input('kx = ')), float(input('ky = '))
+            if True in [e.key == key for key in (pygame.K_1, pygame.K_2, pygame.K_3)]:
+                is_num_pressed = [False, False, False]
 
-            if distance <= point_r and not point_click[0]:
-                point_click[1] = True
-        elif e.type == pygame.MOUSEBUTTONUP and e.button == pygame.BUTTON_LEFT:
-            point_click = [False, False]
+    if is_pressed:
+        if True not in is_num_pressed:
+            coefficients[0] = coefficients[2] = mouse_pos[0] / stop, sqrt(mouse_pos[1]) / stop
+            coefficients[1] = sqrt(mouse_pos[0]) / stop, mouse_pos[1] / stop
+        else:
+            index = is_num_pressed.index(True)
 
-    if point_click[1]:
-        point = [mouse_point[0] - pos[0], mouse_point[1] - pos[1]]
+            coefficients[index] = (
+                mouse_pos[0] / stop, sqrt(mouse_pos[1]) / stop
+            ) if index in (0, 2) else (
+                sqrt(mouse_pos[0]) / stop, mouse_pos[1] / stop
+            )
+
+    point_groups = [[], [], []]
+    super_points = []
+
+    for k in range(start, stop + 1):
+        kx, ky = coefficients[0]
+        point_groups[0].append((k * kx, (k * ky) ** 2))
+        if k < stop / 1.2555 and k % 2 == 0:
+            super_points.append((k * kx / 2, (k * ky) ** 2))
+
+    right_bottom = point_groups[0][-1]
+
+    if coefficients[1][0] is None:
+        coefficients[1] = sqrt(right_bottom[0]) / stop, right_bottom[1] / stop
+
+    for k in range(start, stop + 1):
+        point_groups[1].append(((k * coefficients[1][0]) ** 2, k * coefficients[1][1]))
+
+    points = []
+
+    for k in range(start, stop + 1):
+        points.append((k * coefficients[2][0], (k * coefficients[2][1]) ** 2))
+
+    for point in points[::-1]:
+        point_groups[2].append((points[-1][-2] - point[0], points[-1][-1] - point[1]))
 
     surface.fill(0xffffff)
 
-    draw_parabola(surface, *pos, *point)
+    pygame.draw.line(surface, 0x4caf50, (0, right_bottom[-1]), right_bottom, 3)
+    pygame.draw.line(surface, 0x4caf50, (right_bottom[-2], 0), right_bottom, 3)
+    pygame.draw.line(surface, 0x4caf50, (0, 0), right_bottom, 3)
 
-    pygame.draw.circle(surface, 0x4caf50, (pos[0] + point[0], pos[1] + point[1]), point_r)
+    text_lines = 0
+    for i in range(len(colors)):
+        pygame.draw.lines(surface, colors[i], False, point_groups[i], 3)
+
+        surface.blit(basefont.render(f'pos({i}): {point_groups[i][-1]}', True, 0x000000), (right_bottom[-2], right_bottom[-1] + basefont.get_height() * text_lines))
+        text_lines += 1
+
+        surface.blit(basefont.render(f'coef({i}): {coefficients[i]}', True, 0x000000), (right_bottom[-2], right_bottom[-1] + basefont.get_height() * text_lines))
+        text_lines += 1
+
+    pygame.draw.lines(surface, 0x795548, False, super_points, 3)
 
     display.flip()
     clock.tick(60)
