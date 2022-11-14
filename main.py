d1c1dbf94509d4b14bf3e99663d602701033bc59
TeaCondemns <tea.condemns@gmail.com>
2022-11-14 20:41:57 +0300

    Update main.py
    Code refactored

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from pygex.text import bufferize_font, render_text
-from pygex.mouse import Mouse
-from pygex.input import Input
+from pygame.event import get as get_event
+from pygame.constants import RESIZABLE
 from pygame import display
 from curve import Curve
 from grid import Grid
-import pygame
+from pygex import *
 import theme
 
 
@@ -18,41 +18,28 @@ WIDTH = 1
 
 # Pygame stuff
 display.set_caption("Bezier curve")
-display.set_mode(ANCHOR, pygame.RESIZABLE)
+display.set_mode(ANCHOR, RESIZABLE)
 
 surface = display.get_surface()
-clock = pygame.time.Clock()
 
 # Pygex stuff
 bufferize_font(20)
-
-mouse = Mouse()
-input = Input()
 
 # Core stuff
 curve = Curve(RADIUS)
 grid = Grid(curve, 70, ANCHOR)
 
 while True:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            exit()
-
-        input.process_event(e)
-        mouse.process_event(e)
+    for e in get_event():
+        process_event(e)
 
     grid.prerender()
     curve.prerender()
 
     # Render
-    surface.fill(theme.BG_COLOR)
-
     grid.render(surface, theme.BG_ACCENT_COLOR, theme.BG_NOT_ACCENT_COLOR, display.get_window_size(), WIDTH, RADIUS)
     curve.render(surface, WIDTH)
 
-    surface.blit(render_text(f'fps: {clock.get_fps():.3f}', theme.TEXT_COLOR), (0, 0))
+    surface.blit(render_text(f'fps: {get_clock().get_fps():.3f}', theme.TEXT_COLOR), (0, 0))
 
-    display.flip()
-    input.flip()
-    mouse.flip()
-    clock.tick(60)
+    flip(theme.BG_COLOR, 60)
